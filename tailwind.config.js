6510450856d24956d975f57ede3d23d18f0d9de4
Ersad Ishlahuddin <ersad@pintu.co.id>
2023-02-18 17:49:53 +0700

    fix responsiveness

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,13 @@ module.exports = {
   content: ['*.{html,js}'],
   theme: {
     extend: {
+      screens: {
+        'mx2xl': {'max': '1535px'},  
+        'mxxl': {'max': '1279px'},  
+        'mxlg': {'max': '1023px'},  
+        'mxmd': {'max': '767px'},  
+        'mxsm': {'max': '639px'},
+      },
       fontFamily: {
         routtage: ["Routtage", "normal"],
         rubik: ["Rubik", "sans-serif"],
